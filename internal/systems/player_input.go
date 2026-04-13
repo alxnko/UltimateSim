52c00a6666a3759dffc3e7caebc976ca25ccf907
Alex Neko <89140687+alxnko@users.noreply.github.com>
2026-04-14 00:36:29 +0600

    feat: add mouse click hooks to PlayerInputSystem

--- internal/systems/player_input.go
+++ internal/systems/player_input.go
@@ -1,17 +1,17 @@
 package systems
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/mlange-42/arche/ecs"
 	"github.com/ALXNKO/UltimateSim/internal/components"
 )
 
-// PlayerInputSystem translates real-time user input (WASD) into physical ECS mutations.
 type PlayerInputSystem struct {
 	filter ecs.Filter
 }
 
-// Initialize sets up the Arche-Go filter for Possessed entities.
 func (s *PlayerInputSystem) Initialize(world *ecs.World) {
 	s.filter = ecs.All(
 		ecs.ComponentID[components.Possessed](world),
@@ -19,29 +19,28 @@ func (s *PlayerInputSystem) Initialize(world *ecs.World) {
 	)
 }
 
-// Update reads Ebitengine keyboard states and updates the Velocity of possessed entities.
 func (s *PlayerInputSystem) Update(world *ecs.World) {
 	query := world.Query(s.filter)
 	for query.Next() {
 		vel := (*components.Velocity)(query.Get(ecs.ComponentID[components.Velocity](world)))
 		
-		// Reset velocity to avoid sliding unless keys are pressed
 		vel.X = 0
 		vel.Y = 0
-		
 		speed := float32(2.0)
 		
-		if ebiten.IsKeyPressed(ebiten.KeyW) {
-			vel.Y = -speed
+		if ebiten.IsKeyPressed(ebiten.KeyW) { vel.Y = -speed }
+		if ebiten.IsKeyPressed(ebiten.KeyS) { vel.Y = speed }
+		if ebiten.IsKeyPressed(ebiten.KeyA) { vel.X = -speed }
+		if ebiten.IsKeyPressed(ebiten.KeyD) { vel.X = speed }
+
+		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+			cx, cy := ebiten.CursorPosition()
+			fmt.Printf("Attack Triggered towards screen pos: %d, %d\n", cx, cy)
 		}
-		if ebiten.IsKeyPressed(ebiten.KeyS) {
-			vel.Y = speed
-		}
-		if ebiten.IsKeyPressed(ebiten.KeyA) {
-			vel.X = -speed
-		}
-		if ebiten.IsKeyPressed(ebiten.KeyD) {
-			vel.X = speed
+		
+		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonRight) {
+			cx, cy := ebiten.CursorPosition()
+			fmt.Printf("Interact Triggered towards screen pos: %d, %d\n", cx, cy)
 		}
 	}
 }
